f06290a6805ea8d367c0089c4bbeb521fc50780f
quinnanderson <quinnanderson@vt.edu>
2023-06-15 12:28:15 -0400

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace VTMUNC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Committees()
         {
             return View();
         }
