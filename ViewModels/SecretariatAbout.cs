629ca96c2bc617262459bb9d533228a1ee7d66b5
Erland A Syafiq <syafiqeas@gmail.com>
2023-07-19 15:26:26 -0400

    Stuff

---
+++ ViewModels/SecretariatAbout.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace VTMUNC.ViewModels
+{
+    public class SecretariatAbout
+    {
+        public string? ImagePath { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Position { get; set; }
+
+        public string? Bio { get; set; }
+
+        public string? Email { get; set; }
+
+        public SecretariatAbout(
+            string? imagePath= null, 
+            string? name = null, 
+            string? position = null, 
+            string? bio = null, 
+            string? email = null)
+        {
+            ImagePath = imagePath;
+            Name = name;
+            Position = position;
+            Bio = bio;
+            Email = email;
+        }
+    }
+
+
+
+}
