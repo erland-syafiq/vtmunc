85660f6cd7772d5e64303e80d831027640cc0488
Erland A Syafiq <syafiqeas@gmail.com>
2023-08-14 21:27:03 -0400

    Configure DB & added migration bundle

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using VTMUNC.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
