56eb71a5f2e98130d062e2b28c7b0e341dcdcc12
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-09 01:46:30 -0400

    Reduced Applicant Field Size

--- Models/Applicant.cs
+++ Models/Applicant.cs
@@ -5,25 +5,56 @@ namespace VTMUNC.Models
     public class Applicant
     {
         [Key]
+        [Required]
         public int Id { get; set; }  // must be public!
 
+        [Required]
+        [MaxLength(255)]
         public String AdvisorEmail { get; set; }
-        public String AdvisorName { get; set;}
-        public String AdvisorPhone { get; set;} 
-        public String AdvisorRelation { get; set;}
-        public String AdvisorOtherInformation { get; set;}  
-        public String HeadDelegateEmail { get; set; }
-        public String HeadDelegateName { get; set;}
-        public String HeadDelegatePhone { get; set;}    
-        public String SchoolName { get; set; } 
-        public String DelegationSize { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public String AdvisorName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public String AdvisorPhone { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public String AdvisorRelation { get; set; }
+
+        [MaxLength(1023)]
+        public String AdvisorOtherInformation { get; set; }
+
+        [MaxLength(255)]
+        public String? HeadDelegateEmail { get; set; }
+        [MaxLength(255)]
+        public String? HeadDelegateName { get; set; }
+        [MaxLength(255)]
+        public String? HeadDelegatePhone { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public String SchoolName { get; set; }
+        [Required]
+        [Range(0, 255)]
+        public int DelegationSize { get; set; }
+
+        [Required]
+        [MaxLength(511)]
         public String SchoolMailingAddress { get; set; }
+
+        [MaxLength(511)]
         public String NamesOfDelegates { get; set; }
 
-        //required and needs true
-        //[Required][Range(typeof(bool), "true", "true", ErrorMessage = "You need to agree first!")]
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You need to agree first!")]
         public bool IsAgreeWithTerms { get; set; }
-        public String CommentsOrQuestions { get; set; } 
+
+        [MaxLength(1023)]
+        public String CommentsOrQuestions { get; set; }
 
         public Applicant()
         {
