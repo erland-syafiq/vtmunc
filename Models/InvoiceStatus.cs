5d6059351d7a24dfe12a49c350ce548ba015f829
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-10 23:15:39 -0400

    Changed InvoiceStatus Enum Display Names

--- Models/InvoiceStatus.cs
+++ Models/InvoiceStatus.cs
@@ -1,9 +1,16 @@
-﻿namespace VTMUNC.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace VTMUNC.Models
 {
+
     public enum InvoiceStatus
     {
+        [Display(Name = "Invoice Not Sent")]
         InvoiceNotSent = 0,
+
+        [Display(Name = "Payment Not Received")]
         PaymentNotReceived = 1,
+
+        [Display(Name = "Payment Received")]
         PaymentReceived = 2
     }
 }
