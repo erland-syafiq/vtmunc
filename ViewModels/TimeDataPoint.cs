4c753a29ce230570396595159d1ad215da686351
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-10 22:04:26 -0400

    Graphs Added to Applicant Dashboard

---
+++ ViewModels/TimeDataPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace VTMUNC.ViewModels
+{
+public class TimeDataPoint
+{
+    public String x { get; private set; }
+    public int y { get; private set; }
+
+    public TimeDataPoint(DateTime date, int y)
+    {
+        this.x = date.ToString("yyyy-MM-dd");
+        this.y = y;
+    }
+
+
+}
+}
