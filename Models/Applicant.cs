3ac774d62cd64fad9235db9b3856c88a98a10f41
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-11 00:48:19 -0400

    Added Create

--- Models/Applicant.cs
+++ Models/Applicant.cs
@@ -10,6 +10,7 @@ namespace VTMUNC.Models
         public int Id { get; set; }
 
         [Required]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
