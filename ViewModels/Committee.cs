6021632500ac811ba6d7324c342e9d97112c1c14
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-26 14:53:10 -0400

    Created Committee Model

---
+++ ViewModels/Committee.cs
@@ -0,0 +1,25 @@
+﻿namespace VTMUNC.ViewModels
+{
+    public class Committee
+    {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
+        
+        public string ExecutiveSummary { get; set; }
+
+        public Staff ChairA { get; set; }
+        public Staff ChairB { get; set; }
+
+        public Committee(string name, string imagePath, string executiveSummary, Staff chairA, Staff chairB)
+        {
+            Name = name;
+            ImagePath = imagePath;
+            ExecutiveSummary = executiveSummary;
+            ChairA = chairA;
+            ChairB = chairB;
+        }
+
+
+    }
+}
