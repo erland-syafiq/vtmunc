d40027c8e109ba0d190ac931b6104ae34497c131
Erland A Syafiq <syafiqeas@gmail.com>
2023-09-26 15:35:38 -0400

    Added Committee Controller

---
+++ Controllers/CommitteesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VTMUNC.ViewModels;
+
+namespace VTMUNC.Controllers
+{
+    public class CommitteesController : Controller
+    {
+
+        List<Committee> _committees = new List<Committee>();
+
+        // GET: CommitteesController
+        public ActionResult Index()
+        {
+            return View(_committees);
+        }
+
+        // GET: CommitteesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
