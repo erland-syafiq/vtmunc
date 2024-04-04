f7056bb677e2c43b54af0517d93a82901791b6a3
Erland A Syafiq <syafiqeas@gmail.com>
2024-04-04 14:33:15 -0400

    Fixed navbar fonts

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,17 +8,17 @@ function Navbar() {
             <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light box-shadow mb-3">
                 <div class="container-fluid">
                     <img src="/Images/Logo.png" class="navbar-logo" />
-                    <a class="navbar-brand customFont" asp-area="" asp-controller="Home" asp-action="Index">VTMUNC</a>
+                    <NavLink class="nav-link" to="/">VTMUNC</NavLink>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="navbar-collapse collapse d-sm-inline-flex justify-content-end">
-                        <NavLink class="nav-link customFont" to="/">Home</NavLink>
-                        <NavLink class="nav-link customFont"to="/about"> About Us</NavLink>
-                        <NavLink class="nav-link customFont" to="/committees">Committees</NavLink>
-                        <NavLink class="nav-link customFont" to="/resources">Resources</NavLink>
-                        <NavLink class="nav-link customFont" to="/sponsors"> Sponsors</NavLink>
+                        <NavLink class="nav-link" to="/">Home</NavLink>
+                        <NavLink class="nav-link" to="/about"> About Us</NavLink>
+                        <NavLink class="nav-link" to="/committees">Committees</NavLink>
+                        <NavLink class="nav-link" to="/resources">Resources</NavLink>
+                        <NavLink class="nav-link" to="/sponsors"> Sponsors</NavLink>
                     </div>
                 </div>
             </nav>
