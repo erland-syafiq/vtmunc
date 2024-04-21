6777a1e7be072704a956c15fe130ed2359f678b6
Erland A Syafiq <syafiqeas@gmail.com>
2024-04-20 23:56:26 -0400

    Added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import Home from './pages/Home.jsx';
 import Navbar from './components/Navbar.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
       <Home/>
+      <Footer/>
     </BrowserRouter>
   );
 }
