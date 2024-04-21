94bae0a94a470d88651d1ea5bf4a136b917cf911
Erland A Syafiq <syafiqeas@gmail.com>
2024-04-20 23:38:56 -0400

    Navbar complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,18 +7,18 @@ function Navbar() {
         <header>
             <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light box-shadow mb-3">
                 <div class="container-fluid">
-                    <img src="/Images/Logo.png" class="navbar-logo" />
-                    <NavLink class="nav-link" to="/">VTMUNC</NavLink>
+                    <NavLink to="/"><img src="/Images/Logo.png" class="navbar-logo" /></NavLink>
+                    <NavLink className="nav-link" to="/">VTMUNC</NavLink>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="navbar-collapse collapse d-sm-inline-flex justify-content-end">
-                        <NavLink class="nav-link" to="/">Home</NavLink>
-                        <NavLink class="nav-link" to="/about"> About Us</NavLink>
-                        <NavLink class="nav-link" to="/committees">Committees</NavLink>
-                        <NavLink class="nav-link" to="/resources">Resources</NavLink>
-                        <NavLink class="nav-link" to="/sponsors"> Sponsors</NavLink>
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="/about"> About Us</NavLink>
+                        <NavLink className="nav-link" to="/committees">Committees</NavLink>
+                        <NavLink className="nav-link" to="/resources">Resources</NavLink>
+                        <NavLink className="nav-link" to="/sponsors"> Sponsors</NavLink>
                     </div>
                 </div>
             </nav>
