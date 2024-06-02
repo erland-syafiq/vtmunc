25ea33e6ace70f3294f0f448a2dcf7d64c4f485e
Erland A Syafiq <syafiqeas@gmail.com>
2024-06-02 01:03:06 -0400

    Added caddy file

---
+++ docs/scripts/ec2-setup.sh
@@ -0,0 +1,40 @@
+
+# Installing Caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+
+# create user
+sudo useradd -m -d /home/webapp -s /bin/bash webapp
+sudo usermod -aG sudo webapp 
+sudo passwd -d webapp
+
+# Allow ssh for webapp
+sudo mkdir /home/webapp/.ssh
+sudo cp /home/ubuntu/.ssh/authorized_keys /home/webapp/.ssh/authorized_keys
+
+# Set the correct permissions for the authorized_keys file
+sudo chmod 600 /home/webapp/.ssh/authorized_keys
+sudo chown webapp:webapp /home/webapp/.ssh/authorized_keys
+
+# Install Docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add docker to user group
+sudo usermod -aG docker $USER
