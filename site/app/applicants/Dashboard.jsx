80dd54ff62d9d44e62051ba2e03acfe20d5071d7
Erland A Syafiq <syafiqeas@gmail.com>
2024-06-12 17:53:37 -0400

    Added simple dashbaord metrics

--- site/app/applicants/Dashboard.jsx
+++ site/app/applicants/Dashboard.jsx
@@ -1,8 +1,49 @@
 
 import React from "react";
 
+
+/**
+ * Dashboard Component
+ * @param {Object[]} applicants array of applicants in descending order 
+ * @returns {JSX.ELement} a dashboard with metrics on total number of participants and more
+ */
 export default function Dashboard({applicants}) {
+
+    // Find total number of participants
+    const totalParticipants = applicants.reduce((accumulator, applicant) => accumulator + applicant.delegationSize, 0);
+
+    // Find number of days since last sign up. 
+    const daysSinceSignUp = Math.floor((new Date() - new Date(applicants[0].date)) / (24 * 60 * 60 * 1000));
+
     return (
-        <h2>Dashboard</h2>
+        <section className="Analysis">
+            <div className="row">
+                <div className="col-3">
+                    <div className="card text-center">
+                        <h4>Total Participants</h4>
+                        <h2>{totalParticipants}</h2>
+                    </div>
+                    <div className="card text-center">
+                        <h4>Number of Days Since a Sign Up</h4>
+                        <h2>{daysSinceSignUp}</h2>
+                    </div>
+                </div>
+                <div className="col-6 text-center">
+                    <div className="card">
+                        <canvas id="participantsChart">
+                        </canvas>
+                    </div>
+                    <h4>
+                        Total Number of Delegates
+                    </h4>
+                </div>
+                <div className="col-3">
+                    <div className="card text-center">
+                        <h4>Applicants' Invoice Status</h4>
+                        <canvas id="invoiceStatusChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
--- site/app/applicants/page.jsx
+++ site/app/applicants/page.jsx
@@ -2,51 +2,56 @@ import React from "react";
 import { getApplicants } from "../api/db/dynamodb";
 import Dashboard from "./Dashboard";
 import Link from "next/link";
+import "./DashboardPage.css";
 
 export default async function DashboardPage() {
     try {
         const applicants = await getApplicants();
+        // Sorts applicants in descending order by date in place
+        applicants.sort((a, b) => new Date(b.date) - new Date(a.date));
 
         return (
-            <main className="container dashboard">
-                <h2>Delegations</h2>
-                <Dashboard />
-                <div className="table-responsive">
-                    <table className="table table-sm table-striped">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>School</th>
-                                <th>Advisor Name</th>
-                                <th>Advisor Email</th>
-                                <th>Delegation Size</th>
-                                <th>Invoice Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                applicants.map((applicant) => {
-                                    return (
-                                        <tr>
-                                            <td>{applicant.date}</td>
-                                            <td>{applicant.schoolName}</td>
-                                            <td>{applicant.advisorName}</td>
-                                            <td>{applicant.advisorEmail}</td>
-                                            <td>{applicant.delegationSize}</td>
-                                            <td>{applicant.invoiceStatus}</td>
-                                            <td>
-                                                <Link href={`/applicants/edit/${applicant.id}`}>Edit</Link> |
-                                                <Link href={`/applicants/details/${applicant.id}`}>Details</Link> |
-                                                <Link href={`/applicants/edit/${applicant.id}`}>Delete</Link>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+            <main className="dashboard">
+                <div className="container">
+                    <h2>Delegations</h2>
+                    <Dashboard applicants={applicants}/>
+                    <div className="table-responsive">
+                        <table className="table table-sm table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>School</th>
+                                    <th>Advisor Name</th>
+                                    <th>Advisor Email</th>
+                                    <th>Delegation Size</th>
+                                    <th>Invoice Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    applicants.map((applicant) => {
+                                        return (
+                                            <tr>
+                                                <td>{applicant.date}</td>
+                                                <td>{applicant.schoolName}</td>
+                                                <td>{applicant.advisorName}</td>
+                                                <td>{applicant.advisorEmail}</td>
+                                                <td>{applicant.delegationSize}</td>
+                                                <td>{applicant.invoiceStatus}</td>
+                                                <td>
+                                                    <Link href={`/applicants/edit/${applicant.id}`}>Edit</Link> |
+                                                    <Link href={`/applicants/details/${applicant.id}`}>Details</Link> |
+                                                    <Link href={`/applicants/edit/${applicant.id}`}>Delete</Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                        })
+                                        }
+                            </tbody>
+                        </table>
 
+                    </div>
                 </div>
             </main>
         )
