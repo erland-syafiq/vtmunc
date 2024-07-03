bd6946042d552152b892289fe01a8bd7da65fda0
quinnandersonvt <quinnanderson@vt.edu>
2024-07-03 15:02:49 -0400

    Completed register success page

--- site/app/register/success/page.jsx
+++ site/app/register/success/page.jsx
@@ -1,11 +1,29 @@
+import React from 'react';
+import './RegisterSuccess.css';
 
 export default function RegisterSuccess() {
     return (
-        <main class="Container">
-            <h1>Thank you for registering for VTMUNC!</h1>
-            <p>
-                We will begin processing your data as soon as possible. 
-            </p>
-        </main>
+        <main className="container">
+            <div className='registerSuccess'>
+                <div className='row'>
+                    <div className='col-md-3'></div>
+                    <div className='col-md-6'>
+                        <div className='registerCard'>
+                            <div className='row'>
+                                <h2>Thank you for registering for VTMUNC!</h2>                 
+                            </div>
+
+                            <div>
+                                <img className="registerImg" src={"/Images/AboutUsGroupPhoto.jpg"} />
+                            </div>
+
+                            <h5>We will begin processing your data as soon as possible. Please reach out to chargedaffaires@vtmunc.org if you have any more questions.</h5>
+                        </div>
+                    </div> 
+                    <div className='col-md-3'></div>
+                </div>
+                </div>
+            <div className="registerSuccessBackground"></div>
+        </main>        
     )
 }
